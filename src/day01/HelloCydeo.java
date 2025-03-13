cbcc6e5597a953058c0994061dbff3922448e464
Felipeoliveira888 <feliperocha88@gmail.com>
2025-03-13 19:21:46 -0400

    Update HelloCydeo.java

--- src/day01/HelloCydeo.java
+++ src/day01/HelloCydeo.java
@@ -3,5 +3,6 @@ package day01;
 public class HelloCydeo {
     public static void main(String[] args) {
         System.out.println("Testing Git");
+        System.out.println("Second day of git, learning this thing");
     }
 }
